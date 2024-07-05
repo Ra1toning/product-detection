d87ddca2a4aa1bdc5de34a3d006e40d90f12b918
Bulgan-Erdene Altankhuyag <bulganerdene021@gmail.com>
2024-07-05 12:59:07 +0800

    test

--- src/utils/webcam.js
+++ src/utils/webcam.js
@@ -1,41 +1,36 @@
-/**
- * Class to handle webcam
- */
 export class Webcam {
-  /**
-   * Open webcam and stream it through video tag.
-   * @param {React.MutableRefObject} videoRef video tag reference
-   * @param {function} onLoaded callback function to be called when webcam is open
-   */
-  open = (videoRef, onLoaded) => {
+  open(videoRef, onLoaded, width, height) {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices
         .getUserMedia({
           audio: false,
           video: {
             facingMode: "environment",
+            width: { ideal: width },
+            height: { ideal: height }
           },
         })
         .then((stream) => {
-          window.localStream = stream;
-          videoRef.current.srcObject = stream;
-          videoRef.current.onloadedmetadata = () => {
-            onLoaded();
-          };
+          if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+            videoRef.current.onloadedmetadata = () => {
+              if (videoRef.current) {
+                videoRef.current.play();
+                onLoaded();
+              }
+            };
+          }
         });
     } else alert("Can't open Webcam!");
-  };
+  }
 
-  /**
-   * Close opened webcam.
-   * @param {React.MutableRefObject} videoRef video tag reference
-   */
-  close = (videoRef) => {
-    if (videoRef.current.srcObject) {
-      videoRef.current.srcObject = null;
-      window.localStream.getTracks().forEach((track) => {
+  close(videoRef) {
+    if (videoRef.current && videoRef.current.srcObject) {
+      const tracks = videoRef.current.srcObject.getTracks();
+      tracks.forEach((track) => {
         track.stop();
       });
+      videoRef.current.srcObject = null;
     } else alert("Please open Webcam first!");
-  };
+  }
 }
